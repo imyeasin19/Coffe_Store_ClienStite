cb420fbab9b242b2709a15727a4c816e992174d3
imyeasin19 <yeasin.mia@northsouth.edu>
2025-05-17 18:07:33 +0600

    Coffe store client site setup

---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import Mainlayout from './Layouts/Mainlayout.jsx';
+import Home from './Component/Home.jsx';
+import AddCoffee from './Component/AddCoffee.jsx';
+import UpdateCoffee from './Component/UpdateCoffee.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Mainlayout></Mainlayout>,
+    children: [
+      {
+        index: true,
+        Component: Home
+      },
+      {
+        path:'/addCoffee',
+        Component: AddCoffee
+      },
+      {
+        path: '/updateCoffee',
+        Component: UpdateCoffee
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
